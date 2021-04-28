2a6c234b4ffe2fff7eabba99ceb3de9f8977b3e9
Margarita Medvedeva <margaret_ne@rambler.ru>
2021-04-28 17:07:38 +0300

    add swagger, log

---
+++ src/main/java/clinic/config/SwaggerConfig.java
@@ -0,0 +1,82 @@
+package clinic.config;
+
+import com.google.common.collect.Lists;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.core.Authentication;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Класс конфигурации Swagger
+ */
+@Log4j2
+@Configuration
+@EnableSwagger2
+@Import(springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfig {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String DEFAULT_INCLUDE_PATTERN = "/api/.*";
+
+    @Bean
+    public Docket api() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Clinic backend API",
+                "API until the front appears",
+                "1.0",
+                "",
+                new Contact("Uinny-M", "", ""),
+                "-",
+                "",
+                new ArrayList<>());
+
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo)
+                .pathMapping("/")
+                .produces(Collections.singleton("application/json"))
+                .consumes(Collections.singleton("application/json"))
+                .ignoredParameterTypes(Authentication.class)
+                .useDefaultResponseMessages(false)
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("ru.clinic.controller"))
+                .paths(PathSelectors.any())
+                .build();
+
+        //log.debug("Swagger was started"); //todo подключить логгирование
+        return docket;
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex(DEFAULT_INCLUDE_PATTERN))
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("JWT", authorizationScopes));
+    }
+}
