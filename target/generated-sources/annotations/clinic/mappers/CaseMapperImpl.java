06ba58bf950e8d17195654c5258ced42f2bb9421
Uinny-M <margaret_ne@rambler.ru>
2021-06-01 11:52:55 +0300

    from tomcat to wildfly

--- target/generated-sources/annotations/clinic/mappers/CaseMapperImpl.java
+++ target/generated-sources/annotations/clinic/mappers/CaseMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-05-30T22:15:39+0300",
+    date = "2021-05-31T15:22:05+0300",
     comments = "version: 1.4.1.Final, compiler: javac, environment: Java 13.0.2 (AdoptOpenJDK)"
 )
 @Component
